8d9372c63776c1b428e05acd5337f5c6654d899c
lollo <lollo@localhost>
2012-10-14 21:48:20 +0200

    Added static middleware to user config

--- skeleton/app/config/init.js
+++ skeleton/app/config/init.js
@@ -17,4 +17,7 @@ module.exports = function(app) {
   
   //Fake PUT and DELETE methods by providing a POST field called '_method'
   app.use(express.methodOverride());
+  
+  //Static server
+  app.use('/public', express.static('../public', {maxAge: 86400000}));
 }
